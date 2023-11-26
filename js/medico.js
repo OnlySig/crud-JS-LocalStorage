8f3515773291cd5c18394d8c157910f631ec7d95
Gabriel Castro <72272327+OnlySig@users.noreply.github.com>
2023-11-26 18:27:14 -0300

    modifica medico

--- js/medico.js
+++ js/medico.js
@@ -1,10 +1,9 @@
 const btnAvancar = document.querySelector(".modal__action");
 const modal = document.getElementById("modal");
-const imgMedico = document.querySelector(".modal__select-avatar").attributes[0].nodeValue
+const imgMedico = document.querySelector(".modal__select-avatar")
 const inputDescricao = document.querySelector(".modal__input")
 const nomeMedico = document.querySelector(".modal__select-name")
 const tipoMedico = document.querySelector(".modal__select-function")
-const imgMedicoSrc = document.querySelector(".modal__select-avatar")
 let agendaLocalParse = JSON.parse(localStorage.agendamento) 
 
 inputDescricao.addEventListener("keydown", e => {
@@ -29,7 +28,7 @@ function criaAgendaCompleta(){
             valorHorario: element.valorHorario,
             valorDescricao: descricao,
             nomeMedico: nomeMedico,
-            imgMedico: imgMedico,
+            imgMedico: imgMedico.attributes[0].nodeValue,
             tipoMedico: tipoMedico,
             fimAgenda: false
         }        
@@ -48,23 +47,63 @@ arrows.forEach(arrow => {
     arrow.addEventListener("click", e => {
         if(p < 0) {
             p = 0
+            return
         } else {
             if(e.target.classList[0] === "arrowUp") {
-                p++
+                if(p == 6) {
+                    p = 0
+                } else {
+                    p++
+                }
             } else {
                 p--
             }
         }
         console.log(p)
+        const pathDown = document.querySelector(".pathArrowDown")
+
         if(p == 0) {
+            pathDown.classList.remove("pathArrowDown-active")
             nomeMedico.textContent = "Dr. Sarah Santos"
             tipoMedico.textContent = "Cardiologista"
-            imgMedicoSrc.src = "img/Rectangle 209.png"
+            imgMedico.src = "img/Rectangle 209.png"
         }
 
-        if(p == 1) {
+        if(p == 1) {    
+            pathDown.classList.add("pathArrowDown-active")
             nomeMedico.textContent = "Dr. Carlos Magno"
             tipoMedico.textContent = "Psiquiatra"
+            imgMedico.src = "img/psiquiatra.jpg"
+        }
+
+        if(p == 2) {
+            nomeMedico.textContent = "Dr. Antônio Veigar"
+            tipoMedico.textContent = "Clínico geral"
+            imgMedico.src = "img/clinicoGeral.png"
+        }
+        
+        if(p == 3) {
+            nomeMedico.textContent = "Dr. Cristiana Amolim"
+            tipoMedico.textContent = "Dermatologista"
+            imgMedico.src = "img/dermatologista.jpeg"
+        }
+
+        if(p == 4) {
+            nomeMedico.textContent = "Dr. Amelia Castro Pereira"
+            tipoMedico.textContent = "Endocrinologista"
+            imgMedico.src = "img/endocrinologista.webp"
+        }
+
+        if(p == 5) {
+            nomeMedico.textContent = "Dr. Nathan Philipe"
+            tipoMedico.textContent = "Infectologista"
+            imgMedico.src = "img/infectologista.png"
+        }
+
+        if(p == 6) {
+            nomeMedico.textContent = "Dr. Marco da Costa"
+            tipoMedico.textContent = "Neurologista"
+            imgMedico.src = "img/neurologista.webp"
         }
     })
 })
